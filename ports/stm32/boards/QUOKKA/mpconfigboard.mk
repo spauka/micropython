27658f2a9e3653c6846d95e42d9951d649102fc1
Sebastian <spauka@gmail.com>
2018-12-09 15:41:12 +1100

    Add quokka configuration to micropython

---
+++ ports/stm32/boards/QUOKKA/mpconfigboard.mk
@@ -0,0 +1,9 @@
+MCU_SERIES = f4
+CMSIS_MCU = STM32F411xE
+AF_FILE = boards/stm32f411_af.csv
+LD_FILES = boards/stm32f411.ld boards/common_ifs.ld
+TEXT0_ADDR = 0x08000000
+TEXT1_ADDR = 0x08020000
+
+# Add extra modules dir
+FROZEN_MPY_DIR = boards/QUOKKA/modules
